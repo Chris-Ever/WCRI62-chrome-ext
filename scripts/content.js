fe8b07917903e039660f25bbc97323ecd264884c
Aaron Brown <aaronbrown@MacBook-Pro-2.local>
2023-11-30 13:43:49 -0800

    more songs

--- scripts/content.js
+++ scripts/content.js
@@ -48,15 +48,24 @@ musicPlayer.setAttribute('autoplay', 'autoplay');
 const body = document.querySelector('body');
 body.appendChild(musicPlayer);
 const pirateMusic = document.createElement('source');
-pirateMusic.setAttribute('src', "https://cdn.pixabay.com/download/audio/2023/09/24/audio_70e3d3e84e.mp3?filename=pirate-tavern-full-version-167990.mp3");
+// pirateMusic.setAttribute('src', "https://cdn.pixabay.com/download/audio/2023/09/24/audio_70e3d3e84e.mp3?filename=pirate-tavern-full-version-167990.mp3");
 pirateMusic.setAttribute('type', 'audio/mp3');
 musicPlayer.appendChild(pirateMusic)
 console.log('piratemusic: ', pirateMusic);
 console.log('musicplayer: ', musicPlayer);
 
+const arrSongs = ["https://cdn.pixabay.com/download/audio/2023/09/24/audio_70e3d3e84e.mp3?filename=pirate-tavern-full-version-167990.mp3", 
+                  "https://cdn.pixabay.com/download/audio/2022/05/27/audio_6c19f59b3c.mp3?filename=pirate-adventures-112150.mp3",
+                  "https://cdn.pixabay.com/download/audio/2022/10/20/audio_2250915409.mp3?filename=award-ceremony-nomination-solemn-winner-triumphant-victory-123485.mp3",
+                  "https://cdn.pixabay.com/download/audio/2023/08/23/audio_c7e134a561.mp3?filename=pirates-163389.mp3",
+                  "https://cdn.pixabay.com/audio/2023/06/08/audio_4dd160673b.mp3"
+]
+pirateMusic.setAttribute('src', arrSongs[Math.ceil(Math.random() * arrSongs.length)]);
 document.addEventListener('click', function () {
+  
     musicPlayer.play();
 });
+console.log(arrSongs[Math.ceil(Math.random() * arrSongs.length)]);
 
 // // https://www.youtube.com/watch?v=QHWAzBZ2hDA
 
