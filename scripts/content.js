6a6424ccfdd2dca81140a6251dfbc1b6f5cc76f1
Chris Everett <chris.everett4242@gmail.com>
2023-11-29 19:19:21 -0800

    purple test

---
+++ scripts/content.js
@@ -0,0 +1,12 @@
+/* {
+ font-size: 100%;
+ font-family: Arial;
+ color: purple;
+}
+*/
+
+const everything = document.querySelectorAll('p');
+
+if(everything){
+everything.style.color = 'purple';
+}
